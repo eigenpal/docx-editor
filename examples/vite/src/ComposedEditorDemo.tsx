098ac44ceb7ded44e0000c48e1ec6158cb474866
Jedr Blaszyk <jedrazb@gmail.com>
2026-09-17 15:26:49 +0200

    fix: remove history validation control from demo

--- examples/vite/src/ComposedEditorDemo.tsx
+++ examples/vite/src/ComposedEditorDemo.tsx
@@ -36,7 +36,6 @@ import { AdapterSwitcher } from '../../shared/AdapterSwitcher';
 import { SourceLink } from '../../shared/SourceLink';
 import { ThemeToggle } from './ThemeToggle';
 import { DrawingsE2eBridge } from './DrawingsE2eBridge';
-import { LiveColorPicker } from './LiveColorPicker';
 import { ReviewWritesE2eBridge } from './ReviewWritesE2eBridge';
 import { keepCaret } from './demoButtons';
 import { DemoHeaderButton } from './DemoHeaderButton';
@@ -489,7 +488,6 @@ function EditorChrome({
         </div>
 
         <div className="demo-header__right">
-          <LiveColorPicker />
           <ThemeToggle value={colorMode} onChange={onColorModeChange} />
           <DemoHeaderButton
             variant="primary"
